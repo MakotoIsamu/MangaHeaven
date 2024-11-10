cbc65146bba47eb03e9bafb55adbe2c970a019b2
Dhruv <dhruvvermaw@gmail.com>
2024-11-10 18:19:26 +0530

    added credentials include in context

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { createContext } from 'react'
-import { BACKEND_URI } from '../utils'
+import React, { createContext, useState, useEffect } from 'react';
+import { BACKEND_URI } from '../utils';
 
-const AuthContext = createContext(false)
+export const AuthContext = createContext();
 
-const AuthProvider = ({ children }) => {
-  const [isAuth, setIsAuth] = useState(null)
+export const AuthProvider = ({ children }) => {
+    const [isAuth, setIsAuth] = useState(false);
 
-  useEffect(() => {
-    const handleAuth = async () => {
-      try {
-        const response = await fetch(`${BACKEND_URI}auth/checkAuth`)
-        const data = await response.json()
-        setIsAuth(data.isAuthenticated)
-      } catch (error) {
-        console.error("Error checking authentication", error)
-        setIsAuth(false) // Optionally handle failure to authenticate
-      }
-    }
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const response = await fetch(`${BACKEND_URI}auth/checkAuth`, {
+                    credentials: 'include',
+                });
+                const data = await response.json();
+                setIsAuth(data.isAuthenticated);
+            } catch (error) {
+                console.error("Error checking authentication", error);
+            }
+        };
+        checkAuth();
+    }, []);
 
-    handleAuth()
-  }, [])
-
-  return (
-    <AuthContext.Provider value={{ isAuth, setIsAuth }}>
-      {children}
-    </AuthContext.Provider>
-  )
-}
-
-export { AuthContext, AuthProvider }
+    return (
+        <AuthContext.Provider value={{ isAuth, setIsAuth }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
